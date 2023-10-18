dc0482015fbb250a10171386c95c8df5e250d331
abhixyz1 <irfanhendra90@gmail.com>
2023-10-18 11:25:09 +0700

    Percobaan 1

--- Pemilihan2Percobaan120.java
+++ Pemilihan2Percobaan120.java
@@ -8,13 +8,14 @@ class Pemilihan2Percobaan120{
         System.out.println("Masukkan Tahun : ");
         int tahun = input20.nextInt();
 
-        if ((tahun % 4) ==0) {
-            if ((tahun % 100) !=0)
-                System.out.println("Tahun Kabisat");
-                else {
-                    System.out.println("Bukan Tahun Kabisat");
-                }
-        } else
-           System.out.println("Bukan Tahun kabisat");
+        if ((tahun % 400 ) == 0) {
+            System.out.println("Tahun Kabisat");
+        } else if ((tahun % 100 ==0)){
+            System.out.println("Bukan Tahun Kabisat");
+        } else if ((tahun % 4) == 0) {
+            System.out.println("Tahun kabisat");
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
     }
 }
