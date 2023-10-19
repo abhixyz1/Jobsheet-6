3a05b4261b11fa7d68612d88a5479830f0ba1e60
abhixyz1 <irfanhendra90@gmail.com>
2023-10-19 22:55:42 +0700

    latihan 2

--- latihan220.java
+++ latihan220.java
@@ -7,7 +7,7 @@ public class latihan220 {
         
         Scanner input = new Scanner(System.in);
 
-        double diskon1, diskon2, diskon3, diskon4, diskon5 = 0;
+/*       double diskon1, diskon2, diskon3, diskon4, diskon5 = 0;
 
 
         System.out.println("Masukkan jenis buku :\n1.Kamus \n2.Novel \n3.Selain Kamus & Novel ");
@@ -22,10 +22,10 @@ public class latihan220 {
             diskon2 = 0.02;
             diskon3 = diskon1 + diskon2;
             if (jml_buku > 2){
-            System.out.println("selamat anda mendapatkan diskon sebesar : " + diskon3);
+            System.out.println("Anda mendapatkan diskon : " + diskon3);
             }
             else{
-            System.out.println("anda hanya mendapatkan diskon : " + diskon1);
+            System.out.println("Anda mendapatkan diskon : " + diskon1);
             }
         
         } else if (jenis_buku == 2) {
@@ -36,21 +36,63 @@ public class latihan220 {
             diskon5 = diskon1 + diskon3;
 
             if (jml_buku > 3){
-                System.out.println("Selamat anda mendapatkan diskon" + diskon4);
+                System.out.println("Anda mendapatkan diskon : " + diskon4);
             } else {
-                System.out.println("selamat anda mendapatkan diskon " + diskon5);
+                System.out.println("Anda mendapatkan diskon : " + diskon5);
             }
 
         } else if (jenis_buku == 3) {
             diskon1 = 0.05;
             if (jml_buku > 3){
-                System.out.println("selamat anda mendapatkan diskon : " + diskon1);
+                System.out.println("Anda mendapatkan diskon : " + diskon1);
             } else {
                 System.out.println("Anda tidak mendapatkan diskon");
             }
         } else {
             System.out.println("Input yang anda masukkan salah");
+        } */
+
+
+        // 
+        System.out.println("Masukkan jenis buku :\n1.Kamus \n2.Novel \n3.Selain Kamus & Novel ");
+        System.out.println("Masukkan pilihan : ");
+        int jenis_buku = input.nextInt();
+
+        System.out.println("Masukkan Jumlah Buku yang dibeli : ");
+        int jml_buku = input.nextInt();
+
+        // 
+        double diskon = 0;
+        switch (jenis_buku) {
+            case 1:
+                diskon = 0.1;
+                if (jml_buku > 2) {
+                    diskon += 0.02;
+                }
+                break;
+            case 2:
+                diskon = 0.07;
+                if (jml_buku > 3) {
+                    diskon += 0.02;
+                } else {
+                    diskon += 0.01;
+                }
+                break;
+            case 3:
+                diskon = 0.05;
+                if (jml_buku > 3) {
+                    diskon += 0.05;
+                }
+                break;
+            default:
+                System.out.println("Input yang anda masukkan salah");
+                return;
         }
+
+        // 
+        System.out.println("Anda mendapatkan diskon : " + diskon);
+    
+
 
 
     }
